1a52c5caa95d8b60c65c92add86ed256d3ba7132
Tobias Kunze <r@rixx.de>
2019-02-14 00:10:37 +0100

    Fix method signatures once more

--- src/bot.py
+++ src/bot.py
@@ -22,7 +22,7 @@ def create_jabber_account(user):
 
 def send_help(status):
     help_text = 'Hi! I am a helper bot for the chaos.social jabber server. If you want an account on our jabber server, message me with a message containing the word "register"!\n\nIf you have any questions, feel free to ask leah or rixx.'
-    mastodon.status_reply(to_status=status['id'], help_text)
+    mastodon.status_reply(to_status=status['id'], status=help_text)
 
 
 def parse_message(status):
@@ -40,21 +40,21 @@ def handle_notification(notification):
         send_help(status)
         return
     if status['account']['username'] != status['account']['acct']:
-        mastodon.status_reply(to_status=status['id'], 'Sorry, our Jabber accounts are only available to inhabitants of chaos.social.')
+        mastodon.status_reply(to_status=status['id'], status='Sorry, our Jabber accounts are only available to inhabitants of chaos.social.')
         return
     user = status['account']['username']
     if has_jabber_account(user):
-        mastodon.status_reply(to_status=status['id'], '{}@jabber.chaos.social is already your registered jabber account – you can register only once.'.format(user))
+        mastodon.status_reply(to_status=status['id'], status='{}@jabber.chaos.social is already your registered jabber account – you can register only once.'.format(user))
         return
     try:
         password = create_jabber_account(user)
-        mastodon.status_reply(to_status=status['id'], 'Your Jabber account has been created:\n\nUser: {user}@jabber.chaos.social\nPassword: {password}\n\nPlease change the password soon, using a client like Pidgin.'.format(user=user, password=password), visibility='private')
+        mastodon.status_reply(to_status=status['id'], status='Your Jabber account has been created:\n\nUser: {user}@jabber.chaos.social\nPassword: {password}\n\nPlease change the password soon, using a client like Pidgin.'.format(user=user, password=password), visibility='private')
     except Exception as e:
         if isinstance(e, subprocess.CalledProcessError) and 'That user already exists' in e.output:
-            mastodon.status_reply(to_status=status['id'], '{}@jabber.chaos.social is already your registered jabber account – you can register only once.'.format(user))
+            mastodon.status_reply(to_status=status['id'], status='{}@jabber.chaos.social is already your registered jabber account – you can register only once.'.format(user))
         else:
             print(e)
-            mastodon.status_reply(to_status=status['id'], 'Oops, I could not register your account. Please stay tuned, I will page my admins.', visibility='private')
+            mastodon.status_reply(to_status=status['id'], status='Oops, I could not register your account. Please stay tuned, I will page my admins.', visibility='private')
             mastodon.status_post(status='@rixx Hey, I failed to register an account for "{user}": {e}'.format(user=user, e=str(e)), visibility='private')
 
 
